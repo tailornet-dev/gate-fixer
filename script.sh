057c1d517e2af219400d0c57d948808717b513e7
d.dinatale <d.dinatale@tailornet.it>
2023-01-23 19:03:04 +0100

    cron and duplicate handling

--- script.sh
+++ script.sh
@@ -1,4 +1,20 @@
-echo {} > /home/kerr/service/knownDevices.config
-docker exec gate git fetch && \
-    git checkout package.json package-lock.json \
-    && git clean -fd package.json package-lock.json
+function installCron() {
+    count="$(crontab -l | grep 'docker restart' | wc -l)"
+    if [ $count -eq 0 ]; then
+        crontab -l 2>/dev/null; echo "5 * * * * docker restart instathings-modbus2mqtt" | crontab -
+    fi
+}
+
+installCron
+configfile=/home/kerr/service/knownDevices.config
+if [ ! -f "$configfile" ]; then echo {} > "$configfile"; fi
+echo "fetching repo..."
+docker exec gate git fetch
+if [ $? -ne 0 ]; then
+    echo "error fetching gate git repository"
+    exit 1;
+fi
+docker exec gate git checkout package.json package-lock.json 2>/dev/null
+docker exec gate git clean -fd package.json package-lock.json 2>/dev/null
+
+installCron
